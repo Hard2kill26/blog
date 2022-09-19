e694f2ff08db4bdd6f61ca62a09497ae5eb0c4ae
José Andrés Gonzalez <chiconet26@gmail.com>
2022-09-18 22:43:04 -0400

    Agregado gitignore

--- src/database.js
+++ src/database.js
@@ -1,8 +1,9 @@
 import { connect } from "mongoose";
+import { MONGODB_URI } from "./config";
 
 (async () => {
   try {
-    const db = await connect("mongodb+srv://jandresgonz:64LBdLjO9z87ayi1@jamtest.1ic95ei.mongodb.net/realmadrid");
+    const db = await connect(MONGODB_URI);
     console.log("DB conectada a", db.connection.name);
   } catch (error) {
     console.error(error);
